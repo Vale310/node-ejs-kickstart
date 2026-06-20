ce51dabf3711e75e6068aaae05ff8e95f007f7ab
Vale310 <122951323+Vale310@users.noreply.github.com>
2026-06-20 14:39:13 -0600

    add list/view pattern with user directory and profile pages

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //include Express
 const express = require('express');
 
+const userData = require('./data/test.json');
+
 //server will listen on this port
 const port = 3000;
 
@@ -35,6 +37,17 @@ app.get('/projects',(req,res)=>{
 app.get('/colombia',(req,res)=>{
     let title = "Fun Facts About Colombia";
     res.render('pages/colombia',{'title': title});
+});
+
+//users list URL
+app.get('/users',(req,res)=>{
+    res.render('users/index',{ title: 'User Directory', users: userData });
+});
+
+//users view URL
+app.get('/users/view/:id',(req,res)=>{
+    let id = req.params.id;
+    res.render('users/view',{ title: 'User Profile', user: userData[--id] });
 });
 
 //Set server to listen for requests
